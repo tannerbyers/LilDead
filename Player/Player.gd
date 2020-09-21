49f7d6daa954396303992f3400352b8603323d1d
tannerbyers <programtanner@gmail.com>
2020-09-21 18:00:32 -0400

    Added new run movement for player and wall jumps because wall jumps

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,9 +18,12 @@ func _physics_process(delta):
 	
 	
 	if x_input != 0:
+		sprite.play("run")
 		motion.x += x_input * ACCELERATION * delta
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
 		sprite.flip_h = x_input < 0
+	else:
+		sprite.play("idle")
 
 	motion.y += GRAVITY * delta
 	
